e7d682d8dd1686c3c91a64a74cdcdf3887714a4a
Manuel Rey <>
2021-08-10 12:06:21 -0300

    agrega ejercicio

---
+++ harmonic.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  int n = atoi(argv[1]);
+  double sum = 0.0;
+  for (int i = 1; i <= n; i++) {
+    sum += 1.0 / i;
+  }
+  printf("%f\n", sum );
+  return 0;
+}
